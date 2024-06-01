c52be3b1e2f7cc98d66007fe97c1b8e94144b71d
AnicetChiza <chizaanicet@gmail.com>
2024-06-01 16:06:17 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -98,13 +98,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // Auto-scroll functionality
     setInterval(showNextItem, 9000);
 });
-
-//Changing image position for the product part
-const mainImage = document.querySelector('.main-image');
-const smallImage = document.querySelectorAll('.small-image');
-
-smallImage.forEach((smallImage, index) => {
-    smallImage.onclick = function() {
-        mainImage.src = smallImage.src;
-    };
-});
